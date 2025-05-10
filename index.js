1d912db2b29bdd8add67f1a3380fe8751a9a0ad3
Felipe Soares <140653664+FelipeSoares-code@users.noreply.github.com>
2025-05-10 16:01:50 +0000

    corrigi a logica inicial

--- index.js
+++ index.js
@@ -10,49 +10,45 @@ function somenteSe(a, b){
     return a == b
 }
 
-const variaveis = {
-  p: { descricao: 'metrô funcionar dentro da normalidade', estado: false },
-  q: { descricao: 'passageiro chegar ao destino no horário', estado: false },
-  r: { descricao: 'haver uma greve de funcionários', estado: false },
-  s: { descricao: 'metrô ficar lotado', estado: false },
-  t: { descricao: 'metrô em manutenção', estado: false },
-  u: { descricao: 'ativar transporte alternativo', estado: false },
-  v: { descricao: 'ter excesso de passageiro na estação', estado: false },
-  w: { descricao: 'passageiro ter alta satisfação', estado: false },
-  x: { descricao: 'ter reclamações públicas', estado: false },
-  y: { descricao: 'passageiro sentir desconforto', estado: false }
+const txtVariaveis = {
+    p: 'metrô funcionar dentro da normalidade',
+    q: 'passageiro chegar ao destino no horário',
+    r: 'haver uma greve de funcionários',
+    s: 'metrô ficar lotado',
+    t: 'metrô em manutenção',
+    u: 'ativar transporte alternativo',
+    v: 'ter excesso de passageiro na estação',
+    w: 'passageiro ter alta satisfação',
+    x: 'ter reclamações públicas',
+    y: 'passageiro sentir desconforto'
 };
 
-const validar = (metroEmGreve) => {
-    //variaveis para facilitar a leitura do codigo
-    const { p, q, r, s, t, u, v, w, x, y } = {
-        p: variaveis.p.estado,
-        q: variaveis.q.estado,
-        r: metroEmGreve,
-        s: variaveis.s.estado,
-        t: variaveis.t.estado,
-        u: variaveis.u.estado,
-        v: variaveis.v.estado,
-        w: variaveis.w.estado,
-        x: variaveis.x.estado,
-        y: variaveis.y.estado
-    };
+//variaveis para facilitar a leitura do codigo
+const numVariaveis = txtVariaveis.length;
+const totalLinhas = Math.pow(2, variaveis.length);
 
-    argumento = implica(
-        (
-            implica(p, q) &&
-            implica(r, !q && s) &&
-            implica(t || r, u) &&
-            implica(r && xor(u, !u), !q) &&
-            implica((q && v), (w && x)) &&
-            implica(!q, !w) &&
-            implica(v, s) &&
-            somenteSe(y, s) &&
-            implica(!w || y, x) &&
-            r
-        ),
-        (
-            xor(t, !t)
-        )
-    )
+for (let i = 0; i < totalLinhas; i++) {
+    // cria um número em binário com o tamanho do número de variáveis
+    // e converte para um array de números com zeros a esqueda para completar
+    // Exemplo: 5 -> [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    // isso é usado para criar todas as combinações possíveis de verdadeiro ou falso
+    // com o número de variáveis que temos
+    const bin = i.toString(2).padStart(numVariaveis, '0').split('').map(Number);
+    const [ p, q, r, s, t, u, v, w, x, y ] = bin;
+
+    argumentos = [
+        implica(p, q),
+        implica(r, !q && s),
+        implica(t || r, u),
+        implica(r && xor(u, !u), !q),
+        implica((q && v), (w && x)),
+        implica(!q, !w),
+        implica(v, s),
+        somenteSe(y, s),
+        implica(!w || y, x),
+        r
+    ];
 }
+
+
+
